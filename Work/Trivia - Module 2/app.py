6408cb0d239a2daee5b283bb8ec809f2383a92e5
Sushant Goel <g.sushant.viet.2009@gmail.com>
2015-10-01 15:32:23 +0530

    Add routes file

--- Work/Trivia - Module 2/app.py
+++ Work/Trivia - Module 2/app.py
@@ -1,4 +1,4 @@
-"""
+﻿"""
 This script runs the application using a development server.
 It contains the definition of routes and views for the application.
 """
--- Work/Trivia - Module 2/routes.py
+++ Work/Trivia - Module 2/routes.py
@@ -1,17 +1,33 @@
-from flask import Flask, url_for
+﻿from flask import Flask, url_for
 from app import app
 
-@app.route('/')
-def hello():
-    url = url_for('about');
-    link = '<a href="' + url + '">About us!</a>';
-    return link;
+#@app.route('/')
+#def hello():
+#    url = url_for('about');
+#    link = '<a href="' + url + '">About us!</a>';
+#    return link;
 
-@app.route('/about')
-def about():
-    return 'We are the knights who say Ni!!';
+#@app.route('/about')
+#def about():
+#    return 'We are the knights who say Ni!!';
 
 @app.route('/question/<title>')
 def question(title):
-    message = 'you said ' + title;
+    message = '<h2> You said ' + title + '</h2>';
     return message;
+
+@app.route('/')
+
+def hello():
+    return '''<html>
+                <head>
+                    <title>Hello, world</title>
+                </head>
+                <body>
+                    <h1>Hello, world</h1>
+                </body>
+            </html>''';
+
+@app.route('/Create')
+def create():
+    return '<h2>This is the create page</h2>'
